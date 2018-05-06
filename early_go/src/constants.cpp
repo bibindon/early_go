b7202f053820ceadcc8977bceed8efe561e8b0ba
bibindon <regubukk@gmail.com>
2018-05-06 17:26:23 +0900

    Move constants to cpp from hpp.

---
+++ early_go/src/constants.cpp
@@ -0,0 +1,24 @@
+#include "constants.hpp"
+
+namespace early_go {
+const int constants::WINDOW_WIDTH = 800;
+const int constants::WINDOW_HEIGHT = 600;
+#if defined(_WIN64) && defined(_WIN32)
+const float constants::ANIMATION_SPEED = 0.02f/120;
+#elif !defined(_WIN64) && defined(_WIN32)
+const float constants::ANIMATION_SPEED = 0.02f;
+#endif
+const std::string constants::APP_NAME = "early_go";
+const std::string constants::DATABASE_NAME = ".res";
+const std::string constants::FAILED_TO_READ_X_FILE_MESSAGE =
+    "Xファイルの読み込みに失敗しました";
+const std::string constants::FAILED_TO_CREATE_HARDWARE_MODE_MESSAGE =
+    "正常に起動できませんでした。動作が重くなる可能性があります。";
+const std::string constants::FAILED_TO_CREATE_WINDOW_MESSAGE =
+    "Failed to create a window.";
+const std::string constants::MESH_FILE_NAME = "tiger.x";
+const std::string constants::MESH_FILE_NAME2 = "Dwarf.x";
+const std::string constants::ANIMATION_MESH_FILE_NAME = "RobotArm.x";
+const std::string constants::SKINNED_ANIMATION_MESH_FILE_NAME = "wolf.x";
+const std::string constants::SKINNED_ANIMATION_MESH_FILE_NAME2 = "tiny.x";
+}
--- early_go/src/constants.hpp
+++ early_go/src/constants.hpp
@@ -3,27 +3,20 @@
 #include "stdafx.hpp"
 // TODO: if constants become large, make namespace more nest.
 namespace early_go {
-namespace constants {
-  const static int WINDOW_WIDTH = 800;
-  const static int WINDOW_HEIGHT = 600;
-#if defined(_WIN64) && defined(_WIN32)
-  const static float ANIMATION_SPEED = 0.02f/120;
-#elif !defined(_WIN64) && defined(_WIN32)
-  const static float ANIMATION_SPEED = 0.02f;
-#endif
-  const static std::string APP_NAME = "early_go";
-  const static std::string DATABASE_NAME = ".res";
-  const static std::string FAILED_TO_READ_X_FILE_MESSAGE =
-      "Xファイルの読み込みに失敗しました";
-  const static std::string FAILED_TO_CREATE_HARDWARE_MODE_MESSAGE =
-      "正常に起動できませんでした。動作が重くなる可能性があります。";
-  const static std::string FAILED_TO_CREATE_WINDOW_MESSAGE =
-      "Failed to create a window.";
-  const static std::string MESH_FILE_NAME = "tiger.x";
-  const static std::string MESH_FILE_NAME2 = "Dwarf.x";
-  const static std::string ANIMATION_MESH_FILE_NAME = "RobotArm.x";
-  const static std::string SKINNED_ANIMATION_MESH_FILE_NAME = "wolf.x";
-  const static std::string SKINNED_ANIMATION_MESH_FILE_NAME2 = "tiny.x";
-} /* namespace constants */
+struct constants {
+  const static int         WINDOW_WIDTH;
+  const static int         WINDOW_HEIGHT;
+  const static float       ANIMATION_SPEED;
+  const static std::string APP_NAME;
+  const static std::string DATABASE_NAME;
+  const static std::string FAILED_TO_READ_X_FILE_MESSAGE;
+  const static std::string FAILED_TO_CREATE_HARDWARE_MODE_MESSAGE;
+  const static std::string FAILED_TO_CREATE_WINDOW_MESSAGE;
+  const static std::string MESH_FILE_NAME;
+  const static std::string MESH_FILE_NAME2;
+  const static std::string ANIMATION_MESH_FILE_NAME;
+  const static std::string SKINNED_ANIMATION_MESH_FILE_NAME;
+  const static std::string SKINNED_ANIMATION_MESH_FILE_NAME2;
+};
 } /* namespace early_go */
 #endif
