c740fa1fea3fa11eded019fca82be9e08c578b6b
bibindon <bibindon0814@gmail.com>
2024-03-13 23:45:04 +0900

    Fix release build binary cannot run.

--- early_go/tools/post-build_event.bat
+++ early_go/tools/post-build_event.bat
@@ -16,16 +16,29 @@ IF NOT %ERRORLEVEL% == 0 (
 
 REM Copy dll
 
-IF NOT EXIST "..\x64\Debug\opencv_imgcodecs320d.dll" (
-    COPY /Y "..\packages\opencv.win.native.redist.320.1.1-vs141\build\native\bin\x64\v141\Debug\opencv_imgcodecs320d.dll" "..\x64\Debug\opencv_imgcodecs320d.dll"
-)
+IF %2==Debug (
+    IF NOT EXIST "..\x64\Debug\opencv_imgcodecs320d.dll" (
+        COPY /Y "..\packages\opencv.win.native.redist.320.1.1-vs141\build\native\bin\x64\v141\Debug\opencv_imgcodecs320d.dll" "..\x64\Debug\opencv_imgcodecs320d.dll"
+    )
 
-IF NOT EXIST "..\x64\Debug\opencv_core320d.dll" (
-    COPY /Y "..\packages\opencv.win.native.redist.320.1.1-vs141\build\native\bin\x64\v141\Debug\opencv_core320d.dll" "..\x64\Debug\opencv_core320d.dll"
-)
+    IF NOT EXIST "..\x64\Debug\opencv_core320d.dll" (
+        COPY /Y "..\packages\opencv.win.native.redist.320.1.1-vs141\build\native\bin\x64\v141\Debug\opencv_core320d.dll" "..\x64\Debug\opencv_core320d.dll"
+    )
 
-IF NOT EXIST "..\x64\Debug\opencv_imgproc320d.dll" (
-    COPY /Y "..\packages\opencv.win.native.redist.320.1.1-vs141\build\native\bin\x64\v141\Debug\opencv_imgproc320d.dll" "..\x64\Debug\opencv_imgproc320d.dll"
-)
+    IF NOT EXIST "..\x64\Debug\opencv_imgproc320d.dll" (
+        COPY /Y "..\packages\opencv.win.native.redist.320.1.1-vs141\build\native\bin\x64\v141\Debug\opencv_imgproc320d.dll" "..\x64\Debug\opencv_imgproc320d.dll"
+    )
+) else (
+    IF NOT EXIST "..\x64\Release\opencv_imgcodecs320.dll" (
+        COPY /Y "..\packages\opencv.win.native.redist.320.1.1-vs141\build\native\bin\x64\v141\Release\opencv_imgcodecs320.dll" "..\x64\Release\opencv_imgcodecs320.dll"
+    )
 
+    IF NOT EXIST "..\x64\Release\opencv_core320.dll" (
+        COPY /Y "..\packages\opencv.win.native.redist.320.1.1-vs141\build\native\bin\x64\v141\Release\opencv_core320.dll" "..\x64\Release\opencv_core320.dll"
+    )
+
+    IF NOT EXIST "..\x64\Release\opencv_imgproc320.dll" (
+        COPY /Y "..\packages\opencv.win.native.redist.320.1.1-vs141\build\native\bin\x64\v141\Release\opencv_imgproc320.dll" "..\x64\Release\opencv_imgproc320.dll"
+    ) 
+)
 
