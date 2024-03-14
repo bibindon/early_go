0f9f32d7e1d49f97eda1eecf5d475594e01d0430
bibindon <bibindon0814@gmail.com>
2024-03-14 09:33:21 +0900

    Add comment for not avoiding memory leak.

--- early_go/src/main.cpp
+++ early_go/src/main.cpp
@@ -11,6 +11,8 @@ int WINAPI WinMain(_In_ ::HINSTANCE hinstance,
                    _In_opt_ ::HINSTANCE, _In_ ::LPSTR, _In_ ::INT)
 try {
 #if (defined(DEBUG) || defined(_DEBUG))
+  // OpenCV always output memory leaks.
+  // If filename and line number are not shown, those are OpenCV memory leaks.
   ::_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 #endif
 
