fc9a7aa52c88826d9725b13696c598ceae6b616d
bibindon <bibindon0814@gmail.com>
2024-03-19 12:46:30 +0900

    Beautify source code.

--- early_go/src/inline_macro.hpp
+++ early_go/src/inline_macro.hpp
@@ -5,114 +5,114 @@
 
 namespace early_go
 {
+/*
+ * If a custom deleter for smart-pointer is necessary, use the following
+ * 'custom_deleter' instead of these.
+ *
+ * These template function, like a safe_*****, is for freeing memory. The reason
+ * why the argument is pointer-reference is because of executing the line of
+ * "p = nullptr;" and what executing the line makes the arguments of a client
+ * which uses this function effected. The real purpose of "p = nullptr;" line is
+ * to prevent from a double-free and being used the argument by the client.
+ */
+template <typename T>
+inline void safe_delete(T *&p)
+{
     /*
-     * If a custom deleter for smart-pointer is necessary, use the following
-     * 'custom_deleter' instead of these.
-     *
-     * These template function, like a safe_*****, is for freeing memory. The reason
-     * why the argument is pointer-reference is because of executing the line of
-     * "p = nullptr;" and what executing the line makes the arguments of a client
-     * which uses this function effected. The real purpose of "p = nullptr;" line is
-     * to prevent from a double-free and being used the argument by the client.
+     * A null check is not necessary because the behavior is defined by standards
+     * which to execute 'delete' to nullptr does not do anything.
      */
-    template <typename T>
-    inline void safe_delete(T *&p)
+    delete p;
+    p = nullptr;
+}
+template <typename T>
+inline void safe_delete_array(T *&p)
+{
+    /*
+     * A null check is not necessary because the behavior is defined by standards
+     * which to execute 'delete' to nullptr does not do anything.
+     */
+    delete[] p;
+    p = nullptr;
+}
+template <typename T>
+inline void safe_release(T *&p)
+{
+    if (p)
     {
-        /*
-         * A null check is not necessary because the behavior is defined by standards
-         * which to execute 'delete' to nullptr does not do anything.
-         */
-        delete p;
+        p->Release();
         p = nullptr;
     }
+}
+/*
+ * A custom deleter for smart-pointer. The reason why a function object not
+ * a template function is because not to have to write a type.
+ *
+ * Usages:
+ *
+ *   void sample()
+ *   {
+ *     LPDIRECT3D9 _p_direct3d9{Direct3DCreate9(D3D_SDK_VERSION)};
+ *     std::unique_ptr<LPDIRECT3D9, custom_deleter> up{
+ *         _p_direct3d9, custom_deleter{}};
+ *   }
+ */
+struct custom_deleter
+{
     template <typename T>
-    inline void safe_delete_array(T *&p)
+    void operator()(T p)
     {
         /*
-         * A null check is not necessary because the behavior is defined by standards
-         * which to execute 'delete' to nullptr does not do anything.
+         * A null assignment is not necessary because a null assignment is for
+         * preventing from a segmentation fault by a double-free and a smart pointer
+         * does not free only once and that behavior is the reason why this is used.
          */
-        delete[] p;
-        p = nullptr;
+        p->Release();
     }
-    template <typename T>
-    inline void safe_release(T *&p)
-    {
-        if (p)
-        {
-            p->Release();
-            p = nullptr;
-        }
-    }
-    /*
-     * A custom deleter for smart-pointer. The reason why a function object not
-     * a template function is because not to have to write a type.
-     *
-     * Usages:
-     *
-     *   void sample()
-     *   {
-     *     LPDIRECT3D9 _p_direct3d9{Direct3DCreate9(D3D_SDK_VERSION)};
-     *     std::unique_ptr<LPDIRECT3D9, custom_deleter> up{
-     *         _p_direct3d9, custom_deleter{}};
-     *   }
-     */
-    struct custom_deleter
-    {
-        template <typename T>
-        void operator()(T p)
-        {
-            /*
-             * A null assignment is not necessary because a null assignment is for
-             * preventing from a segmentation fault by a double-free and a smart pointer
-             * does not free only once and that behavior is the reason why this is used.
-             */
-            p->Release();
-        }
-    };
+};
 
-    /*
-     * A class making logging simple.
-     *
-     * Usages:
-     *   early_go::log_liner{} << 42 << std::to_string(42); // 'std::endl' is added
-     *                                                      // automatically.
-     */
-    struct log_liner
-    {
+/*
+ * A class making logging simple.
+ *
+ * Usages:
+ *   early_go::log_liner{} << 42 << std::to_string(42); // 'std::endl' is added
+ *                                                      // automatically.
+ */
+struct log_liner
+{
 #if defined(DEBUG) || defined(_DEBUG)
-        // c'tor 
-        log_liner(const std::string &op = "") : ostringstream_{}, option_{op} {}
+    // c'tor 
+    log_liner(const std::string &op = "") : ostringstream_{}, option_{op} {}
 
-        // A definition of the "<<" operator. 
-        template <typename T>
-        log_liner &operator<<(T a)
+    // A definition of the "<<" operator. 
+    template <typename T>
+    log_liner &operator<<(T a)
+    {
+        ostringstream_ << a;
+        return *this;
+    }
+
+    // d'tor 
+    ~log_liner()
+    {
+        if (option_ != "-n")
         {
-            ostringstream_ << a;
-            return *this;
+            ostringstream_ << std::endl;
         }
+        OutputDebugString(ostringstream_.str().c_str());
+    }
 
-        // d'tor 
-        ~log_liner()
-        {
-            if (option_ != "-n")
-            {
-                ostringstream_ << std::endl;
-            }
-            OutputDebugString(ostringstream_.str().c_str());
-        }
-
-    private:
-        std::ostringstream ostringstream_;
-        const std::string option_;
+private:
+    std::ostringstream ostringstream_;
+    const std::string option_;
 #else
-        template <typename T>
-        log_liner &operator<<(T)
-        {
-            return *this;
-        }
+    template <typename T>
+    log_liner &operator<<(T)
+    {
+        return *this;
+    }
 #endif
-    };
-
+};
 }
 #endif
+
--- early_go/src/main_window.hpp
+++ early_go/src/main_window.hpp
@@ -5,69 +5,69 @@
 namespace early_go
 {
 
-    class mesh;
-    class animation_mesh;
-    class skinned_animation_mesh;
-    class camera;
-    class character;
-    class abstract_mesh;
-    class operation;
-    class hud;
-    class novel;
+class mesh;
+class animation_mesh;
+class skinned_animation_mesh;
+class camera;
+class character;
+class abstract_mesh;
+class operation;
+class hud;
+class novel;
 
+/*
+ * A class that handles basic window processings.
+ */
+class main_window
+{
+public:
     /*
-     * A class that handles basic window processings.
+     * A struct for showing debug strings anytime. The reason why this struct is
+     * not prototype decralation is necessary that a client of this class can look
+     * the 'render_string' function.
      */
-    class main_window
+    struct render_string_object
     {
-    public:
-        /*
-         * A struct for showing debug strings anytime. The reason why this struct is
-         * not prototype decralation is necessary that a client of this class can look
-         * the 'render_string' function.
-         */
-        struct render_string_object
-        {
-            static std::weak_ptr<ID3DXFont> weak_font_;
-            static void render_string(const std::string &, const int &, const int &);
-        };
-
-        explicit main_window(const HINSTANCE &);
-        ~main_window();
-        int operator()();
-        std::shared_ptr<character> get_main_character();
-        std::shared_ptr<character> get_enemy_character();
-        cv::Point get_screen_coodinate(const D3DXVECTOR3 &);
-
-    private:
-        MSG msg_;
-        std::unique_ptr<IDirect3D9, custom_deleter> direct3d9_;
-        D3DPRESENT_PARAMETERS d3d_present_parameters_;
-        std::shared_ptr<IDirect3DDevice9> d3d_device_;
-        std::shared_ptr<ID3DXFont> font_;
-
-        std::shared_ptr<character> early_;
-        std::shared_ptr<character> suo_;
-
-        D3DXVECTOR3 light_direction_;
-        float light_brightness_;
-        std::shared_ptr<camera> camera_;
-
-        void initialize_direct3d(const HWND &);
-        void debug();
-        void render();
-
-        std::shared_ptr<operation> operation_;
-
-        std::shared_ptr<abstract_mesh> animation_mesh_;
-        std::shared_ptr<abstract_mesh> skinned_animation_mesh_;
-        std::shared_ptr<abstract_mesh> skinned_animation_mesh2_;
-        std::shared_ptr<abstract_mesh> mesh_;
-        std::shared_ptr<abstract_mesh> mesh2_;
-
-        std::shared_ptr<hud> hud_;
-
-        std::shared_ptr<novel> novel_;
+        static std::weak_ptr<ID3DXFont> weak_font_;
+        static void render_string(const std::string &, const int &, const int &);
     };
+
+    explicit main_window(const HINSTANCE &);
+    ~main_window();
+    int operator()();
+    std::shared_ptr<character> get_main_character();
+    std::shared_ptr<character> get_enemy_character();
+    cv::Point get_screen_coodinate(const D3DXVECTOR3 &);
+
+private:
+    MSG msg_;
+    std::unique_ptr<IDirect3D9, custom_deleter> direct3d9_;
+    D3DPRESENT_PARAMETERS d3d_present_parameters_;
+    std::shared_ptr<IDirect3DDevice9> d3d_device_;
+    std::shared_ptr<ID3DXFont> font_;
+
+    std::shared_ptr<character> early_;
+    std::shared_ptr<character> suo_;
+
+    D3DXVECTOR3 light_direction_;
+    float light_brightness_;
+    std::shared_ptr<camera> camera_;
+
+    void initialize_direct3d(const HWND &);
+    void debug();
+    void render();
+
+    std::shared_ptr<operation> operation_;
+
+    std::shared_ptr<abstract_mesh> animation_mesh_;
+    std::shared_ptr<abstract_mesh> skinned_animation_mesh_;
+    std::shared_ptr<abstract_mesh> skinned_animation_mesh2_;
+    std::shared_ptr<abstract_mesh> mesh_;
+    std::shared_ptr<abstract_mesh> mesh2_;
+
+    std::shared_ptr<hud> hud_;
+
+    std::shared_ptr<novel> novel_;
+};
 } // namespace early_go 
 #endif
